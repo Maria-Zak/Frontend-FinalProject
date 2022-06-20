8e9721c35362c36d985c23cee807989a90842e67
isolde-r <writetoisolde@gmail.com>
2022-06-20 11:33:56 +0200

    adds navbar

--- Team3/src/app/app-routing.module.ts
+++ Team3/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BioComponent } from './bio/bio.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: HomeComponent
+  },
+  {
+    path: "bio", component: BioComponent
+  },
+  {
+    path: "testimonials", component: TestimonialsComponent
+  },
+  {
+    path: "contact", component: ContactComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
