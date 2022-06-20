3f63daa15d4432c04627fd31b342c33595dfb16a
Maria Zakharova <zakharovamary@gmail.com>
2022-06-20 11:10:31 +0200

    components created

--- Team3/src/app/app.module.ts
+++ Team3/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { BioComponent } from './bio/bio.component';
+import { HeroComponent } from './hero/hero.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    BioComponent,
+    HeroComponent,
+    PortfolioComponent,
+    TestimonialsComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
