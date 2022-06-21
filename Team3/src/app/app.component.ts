3d21fda50a3c3fa9e78b593ea1e7dba70271e1f3
isolde-r <writetoisolde@gmail.com>
2022-06-21 15:44:20 +0200

    fixes link to top

--- Team3/src/app/app.component.ts
+++ Team3/src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Team3';
+  onActivate(event: any) {
+    // window.scroll(0,0);
+  
+    window.scroll({ 
+            top: 0, 
+            left: 0, 
+            behavior: 'smooth' 
+     });
+  
+  }
 }
+
