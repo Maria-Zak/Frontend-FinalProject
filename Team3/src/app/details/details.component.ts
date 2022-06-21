4adcd236f79fa2b1ffdd32f9cfdd4cbaf8073940
Stefan Kanta <stefan.kanta@gmx.at>
2022-06-21 03:47:28 +0200

    prototype created

--- Team3/src/app/details/details.component.ts
+++ Team3/src/app/details/details.component.ts
@@ -12,15 +12,11 @@ import { portfolioPictures, portfolioCategories } from '../portfolio';
 export class DetailsComponent implements OnInit {
   portCat: IPortfolioC[] = this.DS.portCat
   portPics: IPortfolioPic[] = this.DS.portCatTemp;
-  id: number = 0;
+  index: number = 0;
   constructor(private route: ActivatedRoute, public DS: DetailsService) { }
 
   ngOnInit(): void {
     this.DS.chosenCategory(this.DS.categoryC)
-    // this.route.params.subscribe((params: Params) => {
-    //   this.id = +params['picId'];
-    //   this.portPic = this.DS.portCat[this.id];
-    // });
   }
   ngDoCheck(): void {
     this.portPics = this.DS.portCatTemp
@@ -28,5 +24,7 @@ export class DetailsComponent implements OnInit {
   changeCat(aCategory:string) {
     this.DS.categoryC = aCategory;
   }
-
+  changePic(picNumber:number) {
+    this.index = picNumber;
+  }
 }
