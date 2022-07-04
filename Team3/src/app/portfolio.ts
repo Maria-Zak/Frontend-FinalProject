32a9ffd937168b825f90a1b43fffca643bcc0a9c
Maria Zakharova <zakharovamary@gmail.com>
2022-07-04 09:11:42 +0200

    changes

--- Team3/src/app/portfolio.ts
+++ Team3/src/app/portfolio.ts
@@ -1,212 +1,212 @@
 export const portfolioCategories = [
     {
         name: "WEDDINGS",
-        image: "../assets/img/portfolio/wedding_1.jpg"
+        image: "./assets/img/portfolio/wedding_1.jpg"
     } , {
         name: "VACATION",
-        image: "../assets/img/portfolio/vacation_1.jpg"
+        image: "./assets/img/portfolio/vacation_1.jpg"
     } , {
         name: "FINE ART",
-        image: "../assets/img/portfolio/fine_art_1.jpg"
+        image: "./assets/img/portfolio/fine_art_1.jpg"
     } , {
         name: "MATERNITY",
-        image: "../assets/img/portfolio/maternity_1.jpg"
+        image: "./assets/img/portfolio/maternity_1.jpg"
     } , {
         name: "HEADSHOTS",
-        image: "../assets/img/portfolio/headshots_2.jpg"
+        image: "./assets/img/portfolio/headshots_2.jpg"
     } , {
         name: "PORTRAIT",
-        image: "../assets/img/portfolio/portraits_2.jpg"
+        image: "./assets/img/portfolio/portraits_2.jpg"
     } , {
         name: "BOUDOIR",
-        image: "../assets/img/portfolio/boudoir_1.jpg"
+        image: "./assets/img/portfolio/boudoir_1.jpg"
     } , {
         name: "POLEDANCE",
-        image: "../assets/img/portfolio/poledance.jpg"
+        image: "./assets/img/portfolio/poledance.jpg"
     } , {
         name: "WORKSHOPS",
-        image: "../assets/img/portfolio/workshop_1.jpg"
+        image: "./assets/img/portfolio/workshop_1.jpg"
     }
 ]
 export const portfolioPictures = [
     {
         category: "WEDDINGS",
         name: "test0 name",
-        image: "../assets/img/portfolio/wedding_1.jpg",
+        image: "./assets/img/portfolio/wedding_1.jpg",
         description: "test0 descr",
         date: "test0 date"
     } , {
         category: "WEDDINGS",
         name: "test1 name",
-        image: "../assets/img/portfolio/wedding_2.jpg",
+        image: "./assets/img/portfolio/wedding_2.jpg",
         description: "test1 descr",
         date: "test1 date"
     } , {
         category: "WEDDINGS",
         name: "test2 name",
-        image: "../assets/img/portfolio/wedding_3.jpg",
+        image: "./assets/img/portfolio/wedding_3.jpg",
         description: "test2 descr",
         date: "test2 date"
     } , {
         category: "WEDDINGS",
         name: "test2 name",
-        image: "../assets/img/portfolio/wedding_4.jpg",
+        image: "./assets/img/portfolio/wedding_4.jpg",
         description: "test2 descr",
         date: "test2 date"
     } , {
         category: "WEDDINGS",
         name: "test2 name",
-        image: "../assets/img/portfolio/wedding_5.jpg",
+        image: "./assets/img/portfolio/wedding_5.jpg",
         description: "test2 descr",
         date: "test2 date"
     } , {
         category: "WEDDINGS",
         name: "test2 name",
-        image: "../assets/img/portfolio/wedding_6.jpg",
+        image: "./assets/img/portfolio/wedding_6.jpg",
         description: "test2 descr",
         date: "test2 date"
     } , {
         category: "VACATION",
         name: "test0 name ",
-        image: "../assets/img/portfolio/vacation_1.jpg",
+        image: "./assets/img/portfolio/vacation_1.jpg",
         description: "test0 descr",
         date: "test0 date"
     } , {
         category: "VACATION",
         name: "test1 name ",
-        image: "../assets/img/portfolio/vacation_2.jpg",
+        image: "./assets/img/portfolio/vacation_2.jpg",
         description: "test1 descr",
         date: "test1 date"
     } , {
         category: "VACATION",
         name: "test2 name ",
-        image: "../assets/img/portfolio/vacation_3.jpg",
+        image: "./assets/img/portfolio/vacation_3.jpg",
         description: "test2 descr",
         date: "test2 date"
     } , {
         category: "VACATION",
         name: "test2 name ",
-        image: "../assets/img/portfolio/vacation_4.jpg",
+        image: "./assets/img/portfolio/vacation_4.jpg",
         description: "test2 descr",
         date: "test2 date"
     } , {
         category: "FINE ART",
         name: "test0 name ",
-        image: "../assets/img/portfolio/fine_art_1.jpg",
+        image: "./assets/img/portfolio/fine_art_1.jpg",
         description: "test0 descr",
         date: "test0 date"
     } , {
         category: "MATERNITY",
         name: "test0 name ",
-        image: "../assets/img/portfolio/maternity_1.jpg",
+        image: "./assets/img/portfolio/maternity_1.jpg",
         description: "test0 descr",
         date: "test0 date"
     } , {
         category: "MATERNITY",
         name: "test0 name ",
-        image: "../assets/img/portfolio/maternity_2.jpg",
+        image: "./assets/img/portfolio/maternity_2.jpg",
         description: "test0 descr",
         date: "test0 date"
     } , {
         category: "HEADSHOTS",
         name: "test0 name ",
-        image: "../assets/img/portfolio/headshots_1.jpg",
+        image: "./assets/img/portfolio/headshots_1.jpg",
         description: "test0 descr",
         date: "test0 date"
     } , {
         category: "HEADSHOTS",
         name: "test0 name ",
-        image: "../assets/img/portfolio/headshots_2.jpg",
+        image: "./assets/img/portfolio/headshots_2.jpg",
         description: "test0 descr",
         date: "test0 date"
     } , {
         category: "HEADSHOTS",
         name: "test0 name ",
-        image: "../assets/img/portfolio/headshots_3.jpg",
+        image: "./assets/img/portfolio/headshots_3.jpg",
         description: "test0 descr",
         date: "test0 date"
     } , {
         category: "HEADSHOTS",
         name: "test0 name ",
-        image: "../assets/img/portfolio/headshots_4.jpg",
+        image: "./assets/img/portfolio/headshots_4.jpg",
         description: "test0 descr",
         date: "test0 date"
     } , {
         category: "HEADSHOTS",
         name: "test0 name ",
-        image: "../assets/img/portfolio/headshots_5.jpg",
+        image: "./assets/img/portfolio/headshots_5.jpg",
         description: "test0 descr",
         date: "test0 date"
     } , {
         category: "HEADSHOTS",
         name: "test0 name ",
-        image: "../assets/img/portfolio/headshots_6.jpg",
+        image: "./assets/img/portfolio/headshots_6.jpg",
         description: "test0 descr",
         date: "test0 date"
     } , {
         category: "PORTRAIT",
         name: "test0 name ",
-        image: "../assets/img/portfolio/portraits_2.jpg",
+        image: "./assets/img/portfolio/portraits_2.jpg",
         description: "test0 descr",
         date: "test0 date"
     } , {
         category: "PORTRAIT",
         name: "test0 name ",
-        image: "../assets/img/portfolio/portraits_3.jpg",
+        image: "./assets/img/portfolio/portraits_3.jpg",
         description: "test0 descr",
         date: "test0 date"
     } , {
         category: "PORTRAIT",
         name: "test0 name ",
-        image: "../assets/img/portfolio/portraits_4.jpg",
+        image: "./assets/img/portfolio/portraits_4.jpg",
         description: "test0 descr",
         date: "test0 date"
     } , {
         category: "PORTRAIT",
         name: "test0 name ",
-        image: "../assets/img/portfolio/portraits_5.jpg",
+        image: "./assets/img/portfolio/portraits_5.jpg",
         description: "test0 descr",
         date: "test0 date"
     } , {
         category: "PORTRAIT",
         name: "test0 name ",
-        image: "../assets/img/portfolio/portraits_6.jpg",
+        image: "./assets/img/portfolio/portraits_6.jpg",
         description: "test0 descr",
         date: "test0 date"
     } , {
         category: "PORTRAIT",
         name: "test0 name ",
-        image: "../assets/img/portfolio/portraits_7.jpg",
+        image: "./assets/img/portfolio/portraits_7.jpg",
         description: "test0 descr",
         date: "test0 date"
     } , {
         category: "BOUDOIR",
         name: "test0 name ",
-        image: "../assets/img/portfolio/boudoir_1.jpg",
+        image: "./assets/img/portfolio/boudoir_1.jpg",
         description: "test0 descr",
         date: "test0 date"
     } , {
         category: "BOUDOIR",
         name: "test0 name ",
-        image: "../assets/img/portfolio/boudoir_2.jpg",
+        image: "./assets/img/portfolio/boudoir_2.jpg",
         description: "test0 descr",
         date: "test0 date"
     } , {
         category: "BOUDOIR",
         name: "test0 name ",
-        image: "../assets/img/portfolio/boudoir_3.jpg",
+        image: "./assets/img/portfolio/boudoir_3.jpg",
         description: "test0 descr",
         date: "test0 date"
     } , {
         category: "POLEDANCE",
         name: "test0 name ",
-        image: "../assets/img/portfolio/poledance.jpg",
+        image: "./assets/img/portfolio/poledance.jpg",
         description: "test0 descr",
         date: "test0 date"
     } , {
         category: "WORKSHOPS",
         name: "test0 name ",
-        image: "../assets/img/portfolio/workshop_1.jpg",
+        image: "./assets/img/portfolio/workshop_1.jpg",
         description: "test0 descr",
         date: "test0 date"
     }
